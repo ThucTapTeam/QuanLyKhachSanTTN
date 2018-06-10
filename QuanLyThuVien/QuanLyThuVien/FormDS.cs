643b19d063edc932060a7722bb56df8483e45c14
Quang Hải <Quang Hi@DESKTOP-SSCJKUR>
2018-06-11 01:58:34 +0700

    Xong Form Dau Sach,Form CT Phieu Muon,Form Phieu Muon

--- QuanLyThuVien/QuanLyThuVien/FormDS.cs
+++ QuanLyThuVien/QuanLyThuVien/FormDS.cs
@@ -20,62 +20,244 @@ namespace QuanLyThuVien
 
         private void bntDong_Click(object sender, EventArgs e)
         {
-            
+            bntLuu.Enabled = false;
+            bntThem.Enabled = true;
+            bntSua.Enabled = true;
+            bntXoa.Enabled = true;
+            bntHuy.Enabled = false;
         }
-       
+        string strConn = @"Data Source=DESKTOP-SSCJKUR;Initial Catalog=QuanLyThuVien;Integrated Security=True";
+        SqlConnection conn;
+        int chon;
         public void LoadData()
         {
-            
+            SqlDataAdapter da = new SqlDataAdapter("select *from DauSach", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            this.LoadComboboxTG();
+            this.LoadComboboxNXB();
         }
 
         public void LoadComboboxTG()
         {
-           
+            SqlCommand cmd = new SqlCommand("select MaTG from TacGia", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cbbMaTG.DataSource = ds.Tables[0];
+            cbbMaTG.ValueMember = "MaTG";
         }
 
         public void LoadComboboxNXB()
         {
-            
+            SqlCommand cmd = new SqlCommand("select MaNXB from NhaXuatBan", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cbbMaNXB.DataSource = ds.Tables[0];
+            cbbMaNXB.ValueMember = "MaNXB";
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-           
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                txtMaDS.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clMaDS"].Value);
+                txtTenDS.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clTenDS"].Value);
+                cbbMaTG.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clMaTG"].Value);
+                cbbMaNXB.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clMaNXB"].Value);
+                txtSoLuong.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clSoLuong"].Value);
+                txtNXB.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clNXB"].Value);
+            }
         }
 
         private void bntThem_Click(object sender, EventArgs e)
         {
-           
+            txtMaDS.Enabled = false;
+            bntLuu.Enabled = true;
+            bntHuy.Enabled = true;
+            bntThem.Enabled = false;
+            bntSua.Enabled = false;
+            bntXoa.Enabled = false;
+            txtTenDS.Focus();
+            chon = 1;
+            //Sinh mã tự tăng
+            int count1 = 0;
+            count1 = dataGridView1.Rows.Count; //đếm tất cả các dòng trong datagridview rồi gán cho count
+            string c1 = "";
+            int c2 = 0;
+            c1 = Convert.ToString(dataGridView1.Rows[count1 - 2].Cells[1].Value);
+            c2 = Convert.ToInt32((c1.Remove(0, 4)));//loại bỏ kí tự TG
+            if (c2 + 1 < 10)
+            {
+                txtMaDS.Text = "DS110" + (c2 + 1).ToString();
+            }
+            else if (c2 + 1 < 100)
+            {
+                txtMaDS.Text = "DS11" + (c2 + 1).ToString();
+            }
         }
 
         private void bntSua_Click(object sender, EventArgs e)
         {
-            
+            txtMaDS.Enabled = false;
+            bntLuu.Enabled = true;
+            bntHuy.Enabled = true;
+            bntThem.Enabled = false;
+            bntSua.Enabled = false;
+            bntXoa.Enabled = false;
+            chon = 2;
         }
 
         private void bntLuu_Click(object sender, EventArgs e)
         {
-            
+            switch (chon)
+            {
+                case 1:
+                    {
+                        try
+                        {
+                            SqlCommand cmd = new SqlCommand("ThemDS", conn);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            SqlParameter p = new SqlParameter("@MaDS", txtMaDS.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@TenDS", txtTenDS.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@MaTG", cbbMaTG.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@MaNXB", cbbMaNXB.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@SoLuong", txtSoLuong.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@NamXB", txtNXB.Text);
+                            cmd.Parameters.Add(p);
+
+                            // Thực thi thủ tục
+                            int count = cmd.ExecuteNonQuery();
+                            if (count > 0)
+                            {
+                                MessageBox.Show("Đã thêm", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                LoadData();
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Thông tin vừa nhập chưa chính xác.Hãy thử lại", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        try
+                        {
+                            SqlCommand cmd = new SqlCommand("SuaDS", conn);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            SqlParameter p = new SqlParameter("@MaDS", txtMaDS.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@TenDS", txtTenDS.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@MaTG", cbbMaTG.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@MaNXB", cbbMaNXB.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@SoLuong", txtSoLuong.Text);
+                            cmd.Parameters.Add(p);
+
+                            p = new SqlParameter("@NamXB", txtNXB.Text);
+                            cmd.Parameters.Add(p);
+
+                            int count = cmd.ExecuteNonQuery();
+
+                            if (count > 0)
+                            {
+                                MessageBox.Show("Đã sửa", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                LoadData();
+                            }
+                        }
+                        catch { MessageBox.Show("Không thể sửa", "Nontification", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+                        break;
+                    }
+                case 3:
+                    {
+                        try
+                        {
+                            if (MessageBox.Show("Bạn có chắc muốn xóa thông tin này ?", "Nontification", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                            {
+                                SqlCommand cmd = new SqlCommand("XoaDS", conn);
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                SqlParameter p = new SqlParameter("@MaDS", txtMaDS.Text);
+                                cmd.Parameters.Add(p);
+
+                                int count = cmd.ExecuteNonQuery();
+
+                                if (count > 0)
+                                {
+                                    MessageBox.Show("Đã xóa", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    LoadData();
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Không thể xóa", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        break;
+                    }
+            }
+            bntThem.Enabled = true;
+            bntSua.Enabled = true;
+            bntXoa.Enabled = true;
+            bntLuu.Enabled = false;
+            bntHuy.Enabled = false;
+            txtTenDS.Text = "";
+            cbbMaTG.Text = "TG1101";
+            cbbMaNXB.Text = "XB1101";
+            txtSoLuong.Text = "";
+            txtNXB.Text = "";
         }
 
         private void bntXoa_Click(object sender, EventArgs e)
         {
-           
+            txtMaDS.Enabled = false;
+            bntLuu.Enabled = true;
+            bntHuy.Enabled = true;
+            bntThem.Enabled = false;
+            bntSua.Enabled = false;
+            bntXoa.Enabled = false;
+            chon = 3;
         }
 
         private void txtTim_TextChanged(object sender, EventArgs e)
         {
-           
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from DauSach where TenDS like '" + txtTim.Text + "%'", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
         }
 
         private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
         {
-            
+            dataGridView1.Rows[e.RowIndex].Cells["clSTT"].Value = e.RowIndex + 1;
         }
 
         private void FormDS_Load(object sender, EventArgs e)
         {
-            
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            LoadData();
         }
     }
 }
--- QuanLyThuVien/QuanLyThuVien/frmCTMuonTra.cs
+++ QuanLyThuVien/QuanLyThuVien/frmCTMuonTra.cs
@@ -14,17 +14,26 @@ namespace QLTV
 {
     public partial class frmCTMuonTra : Form
     {
-       
+        string strConn = @"Data Source=DESKTOP-SSCJKUR;Initial Catalog=QuanLyThuVien;Integrated Security=True";
+        SqlConnection conn = new SqlConnection();
+        int chon;
         public frmCTMuonTra()
         {
             InitializeComponent();
-            
-           
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            LoadCTMuonTra();
+            btnLuu.Enabled = false;
+            loadDBCombobox();
+
         }
         private void LoadCTMuonTra()
         {
-            
-            
+            SqlDataAdapter da = new SqlDataAdapter("select * from CTMuonTra", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
         }
         private void groupBox1_Enter(object sender, EventArgs e)
         {
@@ -33,32 +42,139 @@ namespace QLTV
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                cboMaPM.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["MaPM"].Value);
+                cboMaSach.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["MaSach"].Value);
+                dtNgayTra.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["NgayTra"].Value);
+                txtTrangThai.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["TrangThai"].Value);
+                rtbGHICHU.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["GhiChu"].Value);
+            }
         }
         public void loadDBCombobox()
         {
-            
+            SqlCommand cmd = new SqlCommand("select MaPM from PhieuMuon", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cboMaPM.DataSource = ds.Tables[0];
+            cboMaPM.ValueMember = "MaPM";
+
+            SqlCommand cmd1 = new SqlCommand("select MaSach from Sach", conn);
+            SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
+            DataSet ds1 = new DataSet();
+            da1.Fill(ds1);
+            cboMaSach.DataSource = ds1.Tables[0];
+            cboMaSach.ValueMember = "MaSach";
         }
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-           
+            btnThem.Enabled = false;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnLuu.Enabled = true;
+            chon = 1;
 
         }
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-           
+            btnThem.Enabled = false;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnLuu.Enabled = true;
+            chon = 2;
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
-            
+            btnThem.Enabled = false;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnLuu.Enabled = true;
+            chon = 3;
         }
 
         private void btnLuu_Click(object sender, EventArgs e)
         {
-            
+            switch (chon)
+            {
+                case 1:
+                    {
+                        SqlCommand cmd = new SqlCommand("Sp_AddCTMuonTra", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        SqlParameter p = new SqlParameter("@MaPM", cboMaPM.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@MaSach", cboMaSach.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@NgayTra", dtNgayTra.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@TrangThai", txtTrangThai.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@GhiChu", rtbGHICHU.Text);
+                        cmd.Parameters.Add(p);
+                        int count = cmd.ExecuteNonQuery();
+                        if (count >= 1)
+                        {
+                            MessageBox.Show("Thêm mới thành công");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể thêm mới");
+                        }
+                        LoadCTMuonTra();
+                        break;
+                    }
+                case 2:
+                    {
+                        SqlCommand cmd = new SqlCommand("Sp_UpdateCTMuonTra", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        SqlParameter p = new SqlParameter("@MaPM", cboMaPM.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@MaSach", cboMaSach.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@NgayTra", dtNgayTra.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@TrangThai", txtTrangThai.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@GhiChu", rtbGHICHU.Text);
+                        cmd.Parameters.Add(p);
+                        int count = cmd.ExecuteNonQuery();
+                        if (count >= 1)
+                        {
+                            MessageBox.Show("Sửa thành công");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể sửa được");
+                        }
+                        LoadCTMuonTra();
+                        break;
+                    }
+                case 3:
+                    {
+                        SqlCommand cmd = new SqlCommand("Sp_DeleteCTMuonTra", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        SqlParameter p = new SqlParameter("@MaPM", cboMaPM.Text);
+                        cmd.Parameters.Add(p);
+                        int count = cmd.ExecuteNonQuery();
+                        if (count >= 1)
+                        {
+                            MessageBox.Show("Xoá thành công");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể xoá được");
+                        }
+                        LoadCTMuonTra();
+                        break;
+                    }
+
+            }
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
         }
 
         private void groupBox1_Enter_1(object sender, EventArgs e)
@@ -73,13 +189,13 @@ namespace QLTV
 
         private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
         {
-
+            dataGridView1.Rows[e.RowIndex].Cells["STT"].Value = e.RowIndex + 1;
         }
 
         private void txtTimKiem_TextChanged(object sender, EventArgs e)
         {
 
         }
-        
+
     }
 }
